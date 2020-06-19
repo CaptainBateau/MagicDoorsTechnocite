c776d86b84eb16aff8544f4995ac38c9a222b4b4
matteohx@gmail.com <matteohx@gmail.com>
2020-06-19 10:47:53 +0200

    test input ovr

---
+++ Assets/InputOVR.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputOVR : MonoBehaviour
+{
+    public Grab _grab;
+
+    private void Update()
+    {
+        if (OVRInput.Get(OVRInput.RawButton.A))
+        {
+            Debug.Log("Jacky");
+            _grab.SetGrab();
+        }
+        if (OVRInput.GetUp(OVRInput.Button.One))
+        {
+            Debug.Log("Michel");
+            _grab.SetGrab(false);
+        }
+    }
+    
+}
