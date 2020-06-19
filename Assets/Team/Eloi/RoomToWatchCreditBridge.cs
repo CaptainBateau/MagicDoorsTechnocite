4121dd8102e453f8db5e446afe178413edb90583
Eloi Stree <eloistree@gmail.com>
2020-06-19 13:28:10 +0200

    Start to work on credit

---
+++ Assets/Team/Eloi/RoomToWatchCreditBridge.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomToWatchCreditBridge : MonoBehaviour
+{
+
+
+    //public CreditBasic [] GetCreditsInSceneDirty() {
+    //    //CHANGE SATURDAY HIT ME IF IT IS NOT CHANGE IN RELEASE
+    //    QuickCredit [] credits= GameObject.FindObjectsOfType<CreditBasic>();
+    //}
+
+   
+}
