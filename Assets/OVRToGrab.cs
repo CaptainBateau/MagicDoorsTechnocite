e97031ae93209c6814ee0e2f4c12fc6c0376eaa1
matteohx@gmail.com <matteohx@gmail.com>
2020-06-19 16:47:59 +0200

    Push tp + grab

---
+++ Assets/OVRToGrab.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OVRToGrab : MonoBehaviour
+{
+    public Grab _grab;
+
+    public OVRInput.Button _grabButton;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(_grabButton))
+        {
+            _grab.SetGrab();
+        }
+        if (OVRInput.GetUp(_grabButton))
+        {
+            _grab.SetGrab(false);
+        }
+    }
+}
